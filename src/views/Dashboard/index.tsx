1073cde2173766cb71f33790575e194b694629e6
Ryan <100581343@student.swin.edu.au>
2019-09-25 12:32:39 +1000

    added point of sales screen

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -3,12 +3,14 @@ import { Switch, Route, Redirect } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import ProductList from './ProductList';
+import PointOfSale from './PointOfSale';
 
 export default () => (
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route exact path='/tables' component={ProductList} />
+            <Route exact path='/pos' component={PointOfSale} />
             <Route path='*'>
                 <Redirect to='/' />
             </Route>
