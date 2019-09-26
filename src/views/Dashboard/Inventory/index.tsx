5512712fc6bcc11dddc8af7ed3ae0f436e994224
Derek <heyzdexy@gmail.com>
2019-09-26 13:48:24 +1000

    Inventory_Basic

---
+++ src/views/Dashboard/Inventory/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>Put Inventory here.</div>
+    )
+}
--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -3,12 +3,14 @@ import { Switch, Route, Redirect } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import ProductList from './ProductList';
+import Inventory from './Inventory';
 
 export default () => (
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route exact path='/tables' component={ProductList} />
+            <Route exact path='/inventory' component={Inventory} />
             <Route path='*'>
                 <Redirect to='/' />
             </Route>
