a2744827597c67a99ba49e2dece402daca7257d8
Derek <heyzdexy@gmail.com>
2019-09-26 15:12:11 +1000

    added firebase

--- src/views/Dashboard/Inventory/index.tsx
+++ src/views/Dashboard/Inventory/index.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import firebase from '../../../firebase';
+import {
+    Grid,
+    FormControl,
+    ButtonGroup,
+    Button,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    Paper,
+    Checkbox,
+    List,
+    ListItem
+} from '@material-ui/core';
+import Navigation from '../../../components/Navigation';
 
 export default () => {
     return (
-        <div>Put Inventory here.</div>
+        <div>
+            <h1>Inventory</h1>
+            <p>Menu Test C - Put Inventory here.</p>
+
+            <FormControl>
+                <ButtonGroup
+                    fullWidth
+                    color='primary'
+                    size='large'
+                >
+                    <Button
+                        variant='contained'
+                        style={{
+                            marginTop: 18
+                        }}
+                        onClick={() => {}}
+                    >
+                        Add Item
+                    </Button>
+                </ButtonGroup>
+            </FormControl>
+
+        </div>
     )
 }
