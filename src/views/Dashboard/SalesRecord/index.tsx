f7245248128e134a8af07894eacf1020ab56c09d
AlexGalle <102097172@student.swin.edu.au>
2019-09-29 20:17:20 +1000

    add sales to record/bootstrap

--- src/views/Dashboard/SalesRecord/index.tsx
+++ src/views/Dashboard/SalesRecord/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Container,
     InputLabel,
@@ -19,20 +19,25 @@ import {
 import Navigation from '../../../components/Navigation';
 import { Color } from '../../../common/'
 import { lightGreen } from '@material-ui/core/colors';
+import AddSales from './AddSales';
 export default () => {
-   
+    const [formModal, setFormModal] = useState(false)
+    const toggleModal = () => setFormModal(!formModal)
     return (
         <Grid container>
             <Grid item md={2}>
                 <Navigation />
             </Grid>
             <Grid item md={10}>
-                <p style={{ textAlign: 'center', fontSize: 35}}>Sales Record</p>
+                <h1 style={{ textAlign: 'center', fontSize: 35}}>Sales Record</h1>
+                <button type="button" id="addButton" style={{position: 'absolute',right: 43,top:20 }} onClick={() => toggleModal()}>Add to Sales Record</button>
                 <Grid style={{ backgroundColor: Color.lightGreen, borderRadius: 10, borderColor: Color.darkBlue , borderWidth: 12 , width: 1100}}>
                     <div id="salesRecord" style={{ textAlign: 'left', fontSize: 20, padding: 20}}>
                         <p>box</p>
                         <p>box</p>
                         <p>box</p>
+                        <p>box</p>
+                        <p>not a box</p>
                         <p>box</p>
                         <p>box</p>
                         <p>box</p>
@@ -41,9 +46,9 @@ export default () => {
                     </div>
                 </Grid>
             </Grid>
+            <AddSales 
+                toggleModal={toggleModal}
+                formModal={formModal}/>
         </Grid>
-    
-
-
     )
 }
