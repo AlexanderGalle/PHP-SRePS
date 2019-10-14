924b2116f72f5117b385aa59a86eb74b4bc54e42
Zeb <zebmuller54@gmail.com>
2019-10-14 22:36:04 +1100

    less db calls, re-uses DisplaySales code

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -7,9 +7,11 @@ import {
   TableRow,
   TableCell,
   TableBody,
-  Paper
+  Paper,
+  useMediaQuery
 } from "@material-ui/core";
 import Navigation from "../../components/Navigation";
+import DisplaySales from "./SalesRecord/DisplaySales"
 import { Color } from "../../common";
 import { textAlign } from "@material-ui/system";
 import firebase from "../../firebase";
@@ -27,72 +29,25 @@ const style = {
 export default () => {
   const [totalSales, setTotalSales] = useState<number>(0);
   const [revenue, setRevenue] = useState<number>(0);
-  const [recentSales, setRecentSales] = useState([{ id: "" }]);
 
-  const GetSales = () => {
-    var newQuantity = 0;
+  const getSummaries = () => {
     firebase
-      .firestore()
-      .collection("salesRecord")
-      .get()
-      .then(snapshot => {
-        snapshot.docs.forEach(doc => {
-          newQuantity += doc.data().quantity;
-          setTotalSales(newQuantity);
-        });
+    .firestore()
+    .collection("salesRecord")
+    .onSnapshot(snapshot => {
+      let revenue = 0;
+      let sales = 0;
+      snapshot.docs.forEach(doc => {
+        sales += doc.data().quantity;
+        revenue += doc.data().price * doc.data().quantity;
       });
-  };
-
-  const GetRevenue = () => {
-    var Revenue = 0;
-    firebase
-      .firestore()
-      .collection("salesRecord")
-      .get()
-      .then(snapshot => {
-        snapshot.docs.forEach(doc => {
-          Revenue += doc.data().price * doc.data().quantity;
-          setRevenue(Revenue);
-        });
-      });
-  };
-
-  const GetRecentSales = () => {
-    firebase
-      .firestore()
-      .collection("salesRecord")
-      .orderBy("date", "desc")
-      .limit(4)
-      .onSnapshot(snapshot => {
-        setRecentSales(
-          snapshot.docs.map(doc => {
-            return {
-              id: doc.id,
-              ...doc.data()
-            };
-          })
-        );
-      });
+      setTotalSales(sales);
+      setRevenue(revenue);      
+    });
   };
 
   useEffect(() => {
-    GetSales();
-    GetRevenue();
-    GetRecentSales();
-
-    firebase
-      .firestore()
-      .collection("salesRecord")
-      .onSnapshot(snapshot => {
-        let changes = snapshot.docChanges();
-
-        changes.forEach(change => {
-          if (change.type == "modified") {
-            GetSales();
-            GetRevenue();
-          }
-        });
-      });
+    getSummaries();
   }, []);
 
   return (
@@ -145,38 +100,7 @@ export default () => {
             Recent Sales
           </h2>
           <div style={{ width: "95%", margin: "auto" }}>
-            <Paper>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Item name</TableCell>
-                    <TableCell>Item price</TableCell>
-                    <TableCell>Qty purchased</TableCell>
-                    <TableCell>Total price</TableCell>
-                    <TableCell>Transaction Date</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {recentSales.map((sale: any) => {
-                    return (
-                      <TableRow key={sale.id}>
-                        <TableCell component="th" scope="row">
-                          {sale.item_name}
-                        </TableCell>
-                        <TableCell>${sale.price}</TableCell>
-                        <TableCell>{sale.quantity}</TableCell>
-                        <TableCell>${sale.quantity * sale.price}</TableCell>
-                        <TableCell>
-                          {sale.date
-                            ? sale.date.toDate().toLocaleDateString("en-AU")
-                            : ""}
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </Paper>
+            <DisplaySales limit = {5} />
           </div>
         </div>
       </Grid>
